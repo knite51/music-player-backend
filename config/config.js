ff9a699f2c39fe12516e1d36be085240f7edaf29
KniteDev <bmose@atbtechsoft.com>
2019-02-10 21:35:08 +0100

    add config file for database connection with different environments

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    url: process.env.DEV_DATABASE_URL,
+    dialet: 'mysql',
+    operatorsAliases: false
+  },
+  test: {
+    url: process.env.TEST_DATABASE_URL,
+    dialet: 'mysql',
+    operatorsAliases: false
+  },
+  travis: {
+    url: process.env.TRAVIS_DATABASE_URL,
+    dialet: 'mysql',
+    logging: false,
+    operatorsAliases: false
+  },
+  production: {
+    url: process.env.PROD_DATABASE_URL,
+    dialet: 'mysql',
+    operatorsAliases: false
+  }
+};
