01e5d0ee882b811679edc5f0c76af615c09b0f3f
KniteDev <bmose@atbtechsoft.com>
2019-02-21 08:15:05 +0100

    correct typo dialet to dialect

--- config/config.js
+++ config/config.js
@@ -3,23 +3,23 @@ require('dotenv').config();
 module.exports = {
   development: {
     url: process.env.DEV_DATABASE_URL,
-    dialet: 'mysql',
+    dialect: 'mysql',
     operatorsAliases: false
   },
   test: {
     url: process.env.TEST_DATABASE_URL,
-    dialet: 'mysql',
+    dialect: 'mysql',
     operatorsAliases: false
   },
   travis: {
     url: process.env.TRAVIS_DATABASE_URL,
-    dialet: 'mysql',
+    dialect: 'mysql',
     logging: false,
     operatorsAliases: false
   },
   production: {
     url: process.env.PROD_DATABASE_URL,
-    dialet: 'mysql',
+    dialect: 'mysql',
     operatorsAliases: false
   }
 };
