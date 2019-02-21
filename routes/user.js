47d26f4d5fc6b2fe8bcbc938969118f214584329
KniteDev <bmose@atbtechsoft.com>
2019-02-21 08:20:28 +0100

    add validation to routes

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,29 @@
 import express from 'express';
 import { userController } from '../controllers';
+import { userValidation } from '../middlewares';
 
 const router = express.Router();
 
 const { createUser } = userController;
+const {
+  checkRequiredUserFields,
+  checkEmptyUserFields,
+  validateUsername,
+  validateEmail,
+  checkIfIdentifierIsInUse,
+  validatePassword
+} = userValidation;
 
-router.route('/register').post(createUser);
+router
+  .route('/register')
+  .post(
+    checkRequiredUserFields,
+    checkEmptyUserFields,
+    validateUsername,
+    validateEmail,
+    checkIfIdentifierIsInUse,
+    validatePassword,
+    createUser
+  );
 
 export default router;
